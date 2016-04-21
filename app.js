62b211f0e73ae74d537340c99f7bd1f0a4a00fad
Nicholas Krushinski <nkrushin@purdue.edu>
2016-04-21 17:13:56 +0000

    Testing Server

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ var app = express();
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next) {
+  res.status(404).send("hello: " + req.originalURL + " " + next);
+});
+
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
