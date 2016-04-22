5fee2a48984ffec9945c8b23624b882e80c8c778
Nicholas Krushinski <nkrushin@purdue.edu>
2016-04-22 04:22:09 +0000

    Added custom stat assignments, more maps, enemies.

--- public/js.js
+++ public/js.js
@@ -12,23 +12,40 @@ MyAPP.controller('MyCtrl', function($rootScope, $scope, $mdDialog,$mdMedia,$loca
         $rootScope.currentLUK=5;
         $rootScope.currentLVL=1;
         $rootScope.currentEXP=0;
+        $rootScope.nextLvlEXP=30;
+        $rootScope.currentSP=5;
         $rootScope.characterMaxHP=50;
+        $rootScope.characterHP=$rootScope.characterMaxHP;
+        $rootScope.currentMoney=0;
         
-        $scope.currentLoc='Town';
-        $scope.imgUrl='/images/statervillage.png';
-    }
+        $scope.goToLocation("Town");
+    };
 
+	$scope.checkLvlUp = function(){
+		if ($rootScope.currentEXP >= $rootScope.nextLvlEXP)
+        {
+        	$rootScope.currentLVL=$rootScope.currentLVL+1;
+        	$rootScope.currentSP=$rootScope.currentSP+5;
+        	$rootScope.currentEXP = $rootScope.currentEXP - $rootScope.nextLvlEXP;
+        	$rootScope.nextLvlEXP = Math.pow($scope.currentLVL,2)*12 + $scope.currentLVL*8 + 10;
+        	$rootScope.characterMaxHP = $rootScope.characterMaxHP+10+Math.ceil($rootScope.currentSTR*0.2);
+        	$mdDialog.show(
+            $mdDialog.alert()
+                .parent(angular.element(document.querySelector('#popupContainer')))
+                .clickOutsideToClose(true)
+
+                .textContent('You leveled up! Go to the characted menu to assign Stat Points!')
+
+                .ok('Cancel')
+        );
+        }
+	};
 
     $scope.goToTown = function() {
         $scope.currentLoc='Town';
         $scope.imgUrl='/images/startervillage.png';
-        if ($scope.currentEXP >= 100)
-        {
-        	$rootScope.currentLVL=$rootScope.currentLVL+1;
-        	$rootScope.currentEXP = $rootScope.currentEXP - 100;
-        	$rootScope.currentSTR = $rootScope.currentSTR+5;
-        	$rootScope.characterMaxHP = $rootScope.characterMaxHP+10+Math.ceil($rootScope.currentSTR*0.2);
-        }
+        $scope.checkLvlUp();
+        $rootScope.characterHP=$rootScope.characterMaxHP;
     };
     $scope.goToOutside = function() {
       $scope.currentLoc='Outside';
@@ -37,21 +54,57 @@ MyAPP.controller('MyCtrl', function($rootScope, $scope, $mdDialog,$mdMedia,$loca
     $scope.goToCapitalRoad1 = function() {
       $scope.currentLoc='CapitalRoad1';
         $scope.imgUrl='/images/roadtocap1.png';
-        $scope.spawnEnemy("slime");
+        
+        var RNG = parseInt(getRandomInt(0,10),10);
+        if (RNG > 8)
+        {
+        	$scope.spawnEnemy("rabbit");
+        }
+        else
+        {
+        	$scope.spawnEnemy("slime");
+    	}
+
     };
     $scope.goToCapitalRoad2 = function() {
       $scope.currentLoc='CapitalRoad2';
         $scope.imgUrl='/images/Outside.png';
-        $scope.spawnEnemy("wolf");
+        
+        var RNG = parseInt(getRandomInt(0,10),10);
+        if (RNG === 0)
+        {
+        	//attacked on entry
+        	$scope.spawnEnemy("slime");
+        	$scope.goToCombat();
+    	}
+    	else if (RNG > 5)
+    	{
+    		$scope.spawnEnemy("wolf");
+		}
+		else
+		{
+			$scope.spawnEnemy("rabbit");
+		}
+
+    };
+    $scope.goToCapitalRoad3 = function() {
+      $scope.currentLoc='CapitalRoad3';
+        $scope.imgUrl='/images/outsidecapital.png';
+		$scope.spawnEnemy("slime");
+
     };
     $scope.goToLocation = function(location) {
-    	if (location.localeCompare("CapitalRoad1")==0 )
+    	if (location.localeCompare("CapitalRoad1")===0 )
     	{
     		$scope.goToCapitalRoad1();
     	}
-    	else if (location.localeCompare("CapitalRoad2")==0 )
+    	else if (location.localeCompare("CapitalRoad2")===0 )
     	{
     		$scope.goToCapitalRoad2();
+    	}
+    	else if (location.localeCompare("CapitalRoad3")===0 )
+    	{
+    		$scope.goToCapitalRoad3();
     	}
     	else
     	{
@@ -60,29 +113,37 @@ MyAPP.controller('MyCtrl', function($rootScope, $scope, $mdDialog,$mdMedia,$loca
     }
     
 	$scope.spawnEnemy = function(monName){
-		if (monName.localeCompare("wolf")==0)
+		if (monName.localeCompare("wolf")===0)
 		{
-			$scope.EnemyEXPreward=28;
-       	 	$scope.EnemyMoneyreward=0;
-       		$scope.EnemyMaxHP=100;
+			$rootScope.EnemyEXPreward=21;
+       	 	$rootScope.EnemyMoneyreward=0;
+       		$scope.EnemyMaxHP=120;
       	  	$scope.EnemyPdef=2;
        	 	$scope.EnemyPatk=12;
       	  	$scope.enemyImgUrl='/images/wolfsmall.png';
    	 	}
+   	 	else if (monName.localeCompare("rabbit")===0)
+   	 	{
+   	 		$rootScope.EnemyEXPreward=6;
+       	 	$rootScope.EnemyMoneyreward=10;
+       		$scope.EnemyMaxHP=65;
+      	  	$scope.EnemyPdef=5;
+       	 	$scope.EnemyPatk=10;
+      	  	$scope.enemyImgUrl='/images/rabbit.png';
+   	 	}
    	 	else
    	 	{
-   	 		$scope.EnemyEXPreward=3;
-       	 	$scope.EnemyMoneyreward=0;
+   	 		$rootScope.EnemyEXPreward=4;
+       	 	$rootScope.EnemyMoneyreward=0;
        		$scope.EnemyMaxHP=50;
-      	  	$scope.EnemyPdef=5;
-       	 	$scope.EnemyPatk=1;
+      	  	$scope.EnemyPdef=2;
+       	 	$scope.EnemyPatk=2;
       	  	$scope.enemyImgUrl='/images/slime.png';
    	 	}
 
 	}
 
     $scope.goToCombat = function() {
-        $scope.characterHP=$scope.characterMaxHP;
         $scope.EnemyHP=$scope.EnemyMaxHP;
         $scope.lastLoc=$scope.currentLoc;
         $scope.imgUrl='images/combat.png';
@@ -92,13 +153,13 @@ MyAPP.controller('MyCtrl', function($rootScope, $scope, $mdDialog,$mdMedia,$loca
     };
 
     $scope.attack = function() {
-        $scope.characterHP=$scope.characterHP-$scope.EnemyPatk;
+        $rootScope.characterHP=$rootScope.characterHP-$scope.EnemyPatk;
         $scope.EnemyHP=$scope.EnemyHP-(parseInt($scope.getNormalAttackDamage(),10)-$scope.EnemyPdef);
-        if ($scope.characterHP <= 0)
+        if ($rootScope.characterHP <= 0)
         {
         	defeat();
         }
-        if ($scope.EnemyHP <= 0) {
+        else if ($scope.EnemyHP <= 0) {
             victory();
         }
     }
@@ -119,13 +180,22 @@ MyAPP.controller('MyCtrl', function($rootScope, $scope, $mdDialog,$mdMedia,$loca
         );
     }
     var victory = function() {
-        $scope.goToLocation($scope.lastLoc);
         $rootScope.currentEXP=$rootScope.currentEXP+$scope.EnemyEXPreward;
         showVict();
+        $scope.goToLocation($scope.lastLoc);
     }
     var defeat = function() {
+    	$mdDialog.show(
+            $mdDialog.alert()
+                .parent(angular.element(document.querySelector('#popupContainer')))
+                .clickOutsideToClose(true)
+
+                .textContent('You have been defeated. Returning to nearest town.')
+
+                .ok('Cancel')
+        );
+        $rootScope.currentEXP=0;
     	$scope.goToLocation("Town");
-        $scope.currentEXP=0;
     }
 
 
@@ -180,6 +250,10 @@ MyAPP.controller('MyCtrl', function($rootScope, $scope, $mdDialog,$mdMedia,$loca
             $scope.customFullscreen = (wantsFullScreen === true);
         });
     };
+    $scope.goToRest = function(ev) {
+        $rootScope.characterHP=$rootScope.characterMaxHP;
+
+	};
 
     $scope.hide = function() {
         $mdDialog.hide();
@@ -189,10 +263,42 @@ MyAPP.controller('MyCtrl', function($rootScope, $scope, $mdDialog,$mdMedia,$loca
         $mdDialog.cancel();
     };
 
+	$scope.addSTR = function() {
+		if ($rootScope.currentSP > 0)
+		{
+			$rootScope.currentSTR = $rootScope.currentSTR + 1;
+			$rootScope.currentSP = $rootScope.currentSP -1;
+		}
+	};
+	$scope.addDEX = function() {
+		if ($rootScope.currentSP > 0)
+		{
+			$rootScope.currentDEX = $rootScope.currentDEX + 1;
+			$rootScope.currentSP = $rootScope.currentSP -1;
+		}
+	};
+	$scope.addINT = function() {
+		if ($rootScope.currentSP > 0)
+		{
+			$rootScope.currentINT = $rootScope.currentINT + 1;
+			$rootScope.currentSP = $rootScope.currentSP -1;
+		}
+	};
+	$scope.addLUK = function() {
+		if ($rootScope.currentSP > 0)
+		{
+			$rootScope.currentLUK = $rootScope.currentLUK + 1;
+			$rootScope.currentSP = $rootScope.currentSP -1;
+		}
+	};
+
     $scope.answer = function(answer) {
         $mdDialog.hide(answer);
     };
 
+	function getRandomInt(min, max) {
+    	return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
 
 
 });
