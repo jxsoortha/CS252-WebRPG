5836180723a494467d59a6b8c57e8776498cf933
Hanjing Xu <xu675@purdue.edu>
2016-04-25 14:19:52 +0000

    client can now talk to server and store character info to the db.

--- app.js
+++ app.js
@@ -7,6 +7,9 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+var ibmdb = require('ibm_db');
+var bodyParser = require('body-parser');
+
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
@@ -15,19 +18,45 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
 
-app.use(function(req, res, next) {
-  res.status(404).send('Sorry cant find that!');
-});
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// serve the files out of ./public as our main files
+
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
+console.log(process.env)
+
 
 // start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
+app.listen(appEnv.port, function() {
 
 	// print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+  
+});
+
+app.post('/addNewCharacter',function(req,res) {
+	var name = req.body.name;
+	var pwd = req.body.pwd;
+	res.end('got');
+	ibmdb.open("DRIVER={DB2};DATABASE=SQLDB;HOSTNAME=75.126.155.153;UID=user17475;PWD=KPXHTqJk0UWO;PORT=50000;PROTOCOL=TCPIP", function (err,conn) {
+	  if (err) return console.log(err);
+	var query_string= "Insert Into character(name,password,statstr,statdex,statint,statluk,exp,level,nextlvlexp,sp,maxhp,maxmp,money) values ('" + name + "','" + pwd + "',1,1,1,1,0,1,30,16,50,20,0)";
+ 	 conn.query(query_string, function (err, data) {
+   		if (err) console.log(err);
+    		else console.log(data);
+
+   		 conn.close(function () {
+    		  console.log('done');
+   	 });
+ 	 });
+	});
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(function(req, res, next) {
+res.status(404).send('Sorry cant find that!');
 });
--- public/js.js
+++ public/js.js
@@ -1,14 +1,38 @@
 var MyAPP = angular.module('MyAPP',['ngMaterial','ngMessages']);
 
 MyAPP.controller('MyCtrl', function($rootScope, $scope, $mdDialog,$mdMedia,$location,$window,$timeout,$http) {
+	
+    function FormHelper() {
+        this.data = "";
+
+        this.append = function(name, val) {
+            if (this.data.length > 0) {
+                this.data += "&";
+            }
+            this.data += encodeURIComponent(name);
+            this.data += "=";
+            this.data += encodeURIComponent(val);
+        };
+    }
 
     $scope.login = function(charName,charPassword) {
 
-        $rootScope.currentCharName=charName;
-        
-        $http.get("LOGIN?" + $rootScope.currentCharName + "?" + charPassword).then(function(response) {
-    });
-        
+        $rootScope.currentCharName = charName;
+        var fh = new FormHelper();
+        fh.append("name",charName);
+        fh.append("pwd",charPassword);
+        $http({
+            method: 'POST',
+            url: '/addNewCharacter',
+            data: fh.data,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).success(function(data) {
+            console.log(data);
+        }).error(function(data,status) {
+  			console.log(status);      	
+        });
+
+
         $rootScope.currentSTR=1;
         $rootScope.currentDEX=1;
         $rootScope.currentINT=1;
@@ -22,39 +46,39 @@ MyAPP.controller('MyCtrl', function($rootScope, $scope, $mdDialog,$mdMedia,$loca
         $rootScope.characterHP=$rootScope.characterMaxHP;
         $rootScope.characterMP=$rootScope.characterMaxMP=$rootScope.characterMaxMP;
         $rootScope.currentMoney=0;
-        
+
         $scope.goToLocation("Town");
         $mdDialog.show(
-        $mdDialog.alert()
+            $mdDialog.alert()
                 .parent(angular.element(document.querySelector('#popupContainer')))
                 .clickOutsideToClose(true)
 
                 .textContent('Welcome! Please use the menu button on the top right corner to assign stat points(SP) to your character!')
 
                 .ok('Cancel')
-            );
+        );
 
     };
 
-	$scope.checkLvlUp = function(){
-		if ($rootScope.currentEXP >= $rootScope.nextLvlEXP)
+    $scope.checkLvlUp = function(){
+        if ($rootScope.currentEXP >= $rootScope.nextLvlEXP)
         {
-        	$rootScope.currentLVL=$rootScope.currentLVL+1;
-        	$rootScope.currentSP=$rootScope.currentSP+5;
-        	$rootScope.currentEXP = $rootScope.currentEXP - $rootScope.nextLvlEXP;
-        	$rootScope.nextLvlEXP = Math.pow($scope.currentLVL,2)*12 + $scope.currentLVL*8 + 10;
-        	$rootScope.characterMaxHP = $rootScope.characterMaxHP+10+Math.ceil($rootScope.currentSTR*0.2);
-        	$mdDialog.show(
-            $mdDialog.alert()
-                .parent(angular.element(document.querySelector('#popupContainer')))
-                .clickOutsideToClose(true)
+            $rootScope.currentLVL=$rootScope.currentLVL+1;
+            $rootScope.currentSP=$rootScope.currentSP+5;
+            $rootScope.currentEXP = $rootScope.currentEXP - $rootScope.nextLvlEXP;
+            $rootScope.nextLvlEXP = Math.pow($scope.currentLVL,2)*12 + $scope.currentLVL*8 + 10;
+            $rootScope.characterMaxHP = $rootScope.characterMaxHP+10+Math.ceil($rootScope.currentSTR*0.2);
+            $mdDialog.show(
+                $mdDialog.alert()
+                    .parent(angular.element(document.querySelector('#popupContainer')))
+                    .clickOutsideToClose(true)
 
-                .textContent('You leveled up! Go to the characted menu to assign Stat Points!')
+                    .textContent('You leveled up! Go to the characted menu to assign Stat Points!')
 
-                .ok('Cancel')
-        );
+                    .ok('Cancel')
+            );
         }
-	};
+    };
 
     $scope.goToTown = function() {
         $scope.currentLoc='Town';
@@ -63,128 +87,128 @@ MyAPP.controller('MyCtrl', function($rootScope, $scope, $mdDialog,$mdMedia,$loca
         $rootScope.characterHP=$rootScope.characterMaxHP;
     };
     $scope.goToOutside = function() {
-      $scope.currentLoc='Outside';
+        $scope.currentLoc='Outside';
         $scope.imgUrl='/images/Outside.png';
     };
     $scope.goToCapitalRoad1 = function() {
-      $scope.currentLoc='CapitalRoad1';
+        $scope.currentLoc='CapitalRoad1';
         $scope.imgUrl='/images/roadtocap1.png';
-        
+
         var RNG = parseInt(getRandomInt(0,10),10);
         if (RNG > 5)
         {
-        	$scope.spawnEnemy("greenslime");
+            $scope.spawnEnemy("greenslime");
         }
         else
         {
-        	$scope.spawnEnemy("slime");
-    	}
+            $scope.spawnEnemy("slime");
+        }
 
     };
     $scope.goToCapitalRoad2 = function() {
-      $scope.currentLoc='CapitalRoad2';
+        $scope.currentLoc='CapitalRoad2';
         $scope.imgUrl='/images/caproad2.png';
-        
+
         var RNG = parseInt(getRandomInt(0,10),10);
         if (RNG === 0)
         {
-        	//attacked on entry
-        	$scope.spawnEnemy("slime");
-        	$scope.goToCombat();
-        	$mdDialog.show(
-        	$mdDialog.alert()
-                .parent(angular.element(document.querySelector('#popupContainer')))
-                .clickOutsideToClose(true)
+            //attacked on entry
+            $scope.spawnEnemy("slime");
+            $scope.goToCombat();
+            $mdDialog.show(
+                $mdDialog.alert()
+                    .parent(angular.element(document.querySelector('#popupContainer')))
+                    .clickOutsideToClose(true)
 
-                .textContent('You have been ambushed!')
+                    .textContent('You have been ambushed!')
 
-                .ok('Cancel')
+                    .ok('Cancel')
             );
-    	}
-    	else if (RNG > 5)
-    	{
-    		$scope.spawnEnemy("gslime");
-		}
-		else
-		{
-			$scope.spawnEnemy("slime");
-		}
+        }
+        else if (RNG > 5)
+        {
+            $scope.spawnEnemy("gslime");
+        }
+        else
+        {
+            $scope.spawnEnemy("slime");
+        }
 
     };
     $scope.goToLake = function() {
-      $scope.currentLoc='Lake';
+        $scope.currentLoc='Lake';
         $scope.imgUrl='/images/lake.png';
-		$scope.spawnEnemy("wolf");
+        $scope.spawnEnemy("wolf");
 
     };
     $scope.goToCapitalRoad3 = function() {
-      $scope.currentLoc='CapitalRoad3';
+        $scope.currentLoc='CapitalRoad3';
         $scope.imgUrl='/images/outsidecapital.png';
-		$scope.spawnEnemy("slime");
+        $scope.spawnEnemy("slime");
 
     };
     $scope.goToLocation = function(location) {
-    	if (location.localeCompare("CapitalRoad1")===0 )
-    	{
-    		$scope.goToCapitalRoad1();
-    	}
-    	else if (location.localeCompare("CapitalRoad2")===0 )
-    	{
-    		$scope.goToCapitalRoad2();
-    	}
-    	else if (location.localeCompare("Lake")===0 )
-    	{
-    		$scope.goToLake;
-    	}
-    	else if (location.localeCompare("CapitalRoad3")===0 )
-    	{
-    		$scope.goToCapitalRoad3();
-    	}
-    	else
-    	{
-			$scope.goToTown();
-    	}
+        if (location.localeCompare("CapitalRoad1")===0 )
+        {
+            $scope.goToCapitalRoad1();
+        }
+        else if (location.localeCompare("CapitalRoad2")===0 )
+        {
+            $scope.goToCapitalRoad2();
+        }
+        else if (location.localeCompare("Lake")===0 )
+        {
+            $scope.goToLake;
+        }
+        else if (location.localeCompare("CapitalRoad3")===0 )
+        {
+            $scope.goToCapitalRoad3();
+        }
+        else
+        {
+            $scope.goToTown();
+        }
     }
-    
-	$scope.spawnEnemy = function(monName){
-		if (monName.localeCompare("wolf")===0)
-		{
-			$rootScope.EnemyEXPreward=21;
-       	 	$rootScope.EnemyMoneyreward=0;
-       		$scope.EnemyMaxHP=100;
-      	  	$scope.EnemyPdef=1;
-       	 	$scope.EnemyPatk=12;
-      	  	$scope.enemyImgUrl='/images/wolfsmall.png';
-   	 	}
-   	 	else if (monName.localeCompare("rabbit")===0)
-   	 	{
-   	 		$rootScope.EnemyEXPreward=12;
-       	 	$rootScope.EnemyMoneyreward=parseInt(getRandomInt(10,250),10);
-       		$scope.EnemyMaxHP=40;
-      	  	$scope.EnemyPdef=1;
-       	 	$scope.EnemyPatk=5;
-      	  	$scope.enemyImgUrl='/images/rabbit.png';
-   	 	}
-   	 	else if (monName.localeCompare("greenslime")===0)
-   	 	{
-   	 		$rootScope.EnemyEXPreward=15;
-       	 	$rootScope.EnemyMoneyreward=parseInt(getRandomInt(10,20),10);
-       		$scope.EnemyMaxHP=45;
-      	  	$scope.EnemyPdef=5;
-       	 	$scope.EnemyPatk=5;
-      	  	$scope.enemyImgUrl='/images/gslime.png';
-   	 	}
-   	 	else
-   	 	{
-   	 		$rootScope.EnemyEXPreward=6;
-       	 	$rootScope.EnemyMoneyreward=0;
-       		$scope.EnemyMaxHP=22;
-      	  	$scope.EnemyPdef=2;
-       	 	$scope.EnemyPatk=2;
-      	  	$scope.enemyImgUrl='/images/slime.png';
-   	 	}
 
-	}
+    $scope.spawnEnemy = function(monName){
+        if (monName.localeCompare("wolf")===0)
+        {
+            $rootScope.EnemyEXPreward=21;
+            $rootScope.EnemyMoneyreward=0;
+            $scope.EnemyMaxHP=100;
+            $scope.EnemyPdef=1;
+            $scope.EnemyPatk=12;
+            $scope.enemyImgUrl='/images/wolfsmall.png';
+        }
+        else if (monName.localeCompare("rabbit")===0)
+        {
+            $rootScope.EnemyEXPreward=12;
+            $rootScope.EnemyMoneyreward=parseInt(getRandomInt(10,250),10);
+            $scope.EnemyMaxHP=40;
+            $scope.EnemyPdef=1;
+            $scope.EnemyPatk=5;
+            $scope.enemyImgUrl='/images/rabbit.png';
+        }
+        else if (monName.localeCompare("greenslime")===0)
+        {
+            $rootScope.EnemyEXPreward=15;
+            $rootScope.EnemyMoneyreward=parseInt(getRandomInt(10,20),10);
+            $scope.EnemyMaxHP=45;
+            $scope.EnemyPdef=5;
+            $scope.EnemyPatk=5;
+            $scope.enemyImgUrl='/images/gslime.png';
+        }
+        else
+        {
+            $rootScope.EnemyEXPreward=6;
+            $rootScope.EnemyMoneyreward=0;
+            $scope.EnemyMaxHP=22;
+            $scope.EnemyPdef=2;
+            $scope.EnemyPatk=2;
+            $scope.enemyImgUrl='/images/slime.png';
+        }
+
+    }
 
     $scope.goToCombat = function() {
         $scope.EnemyHP=$scope.EnemyMaxHP;
@@ -200,19 +224,19 @@ MyAPP.controller('MyCtrl', function($rootScope, $scope, $mdDialog,$mdMedia,$loca
         $scope.EnemyHP=$scope.EnemyHP-(parseInt($scope.getNormalAttackDamage(),10)-$scope.EnemyPdef);
         if ($rootScope.characterHP <= 0)
         {
-        	defeat();
+            defeat();
         }
         else if ($scope.EnemyHP <= 0) {
             victory();
         }
     }
-	$scope.getNormalAttackDamage = function(){
-		var low = Math.ceil(0.01*$rootScope.currentSTR+0.25*$rootScope.currentDEX);
-		var high = Math.ceil($rootScope.currentSTR+0.5*$rootScope.currentDEX);
-		return parseInt(getRandomInt(low,high),10);
-	}
+    $scope.getNormalAttackDamage = function(){
+        var low = Math.ceil(0.01*$rootScope.currentSTR+0.25*$rootScope.currentDEX);
+        var high = Math.ceil($rootScope.currentSTR+0.5*$rootScope.currentDEX);
+        return parseInt(getRandomInt(low,high),10);
+    }
     $scope.run = function() {
-    	$scope.goToLocation($scope.lastLoc);
+        $scope.goToLocation($scope.lastLoc);
         $mdDialog.show(
             $mdDialog.alert()
                 .parent(angular.element(document.querySelector('#popupContainer')))
@@ -230,7 +254,7 @@ MyAPP.controller('MyCtrl', function($rootScope, $scope, $mdDialog,$mdMedia,$loca
         $scope.goToLocation($scope.lastLoc);
     }
     var defeat = function() {
-    	$mdDialog.show(
+        $mdDialog.show(
             $mdDialog.alert()
                 .parent(angular.element(document.querySelector('#popupContainer')))
                 .clickOutsideToClose(true)
@@ -240,7 +264,7 @@ MyAPP.controller('MyCtrl', function($rootScope, $scope, $mdDialog,$mdMedia,$loca
                 .ok('Cancel')
         );
         $rootScope.currentEXP=0;
-    	$scope.goToLocation("Town");
+        $scope.goToLocation("Town");
     }
 
 
@@ -298,7 +322,7 @@ MyAPP.controller('MyCtrl', function($rootScope, $scope, $mdDialog,$mdMedia,$loca
     $scope.goToRest = function(ev) {
         $rootScope.characterHP=$rootScope.characterMaxHP;
 
-	};
+    };
 
     $scope.hide = function() {
         $mdDialog.hide();
@@ -308,42 +332,42 @@ MyAPP.controller('MyCtrl', function($rootScope, $scope, $mdDialog,$mdMedia,$loca
         $mdDialog.cancel();
     };
 
-	$scope.addSTR = function() {
-		if ($rootScope.currentSP > 0)
-		{
-			$rootScope.currentSTR = $rootScope.currentSTR + 1;
-			$rootScope.currentSP = $rootScope.currentSP -1;
-		}
-	};
-	$scope.addDEX = function() {
-		if ($rootScope.currentSP > 0)
-		{
-			$rootScope.currentDEX = $rootScope.currentDEX + 1;
-			$rootScope.currentSP = $rootScope.currentSP -1;
-		}
-	};
-	$scope.addINT = function() {
-		if ($rootScope.currentSP > 0)
-		{
-			$rootScope.currentINT = $rootScope.currentINT + 1;
-			$rootScope.currentSP = $rootScope.currentSP -1;
-		}
-	};
-	$scope.addLUK = function() {
-		if ($rootScope.currentSP > 0)
-		{
-			$rootScope.currentLUK = $rootScope.currentLUK + 1;
-			$rootScope.currentSP = $rootScope.currentSP -1;
-		}
-	};
+    $scope.addSTR = function() {
+        if ($rootScope.currentSP > 0)
+        {
+            $rootScope.currentSTR = $rootScope.currentSTR + 1;
+            $rootScope.currentSP = $rootScope.currentSP -1;
+        }
+    };
+    $scope.addDEX = function() {
+        if ($rootScope.currentSP > 0)
+        {
+            $rootScope.currentDEX = $rootScope.currentDEX + 1;
+            $rootScope.currentSP = $rootScope.currentSP -1;
+        }
+    };
+    $scope.addINT = function() {
+        if ($rootScope.currentSP > 0)
+        {
+            $rootScope.currentINT = $rootScope.currentINT + 1;
+            $rootScope.currentSP = $rootScope.currentSP -1;
+        }
+    };
+    $scope.addLUK = function() {
+        if ($rootScope.currentSP > 0)
+        {
+            $rootScope.currentLUK = $rootScope.currentLUK + 1;
+            $rootScope.currentSP = $rootScope.currentSP -1;
+        }
+    };
 
     $scope.answer = function(answer) {
         $mdDialog.hide(answer);
     };
 
-	function getRandomInt(min, max) {
-    	return Math.floor(Math.random() * (max - min + 1)) + min;
-	}
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 
 
 });
